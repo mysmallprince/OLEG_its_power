8432064b2490674c7e554ca65848f3339741d08f
mysmallprince <71024112+mysmallprince@users.noreply.github.com>
2020-09-14 21:43:22 +0300

    test10

---
+++ New Unity Project (2)/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+
+    public GameObject btnLeft;
+    public GameObject btnRight;
+    float PosBtnLeft;
+    float PosBtnRight;
+    float run;
+
+
+    void Start()
+    {
+
+        PosBtnLeft = btnLeft.transform.position.y;
+        PosBtnRight = btnRight.transform.position.y;
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        if (PosBtnLeft != btnLeft.transform.position.y)
+        {
+            run = -3f;
+        }
+
+        else if (PosBtnRight != btnRight.transform.position.y)
+        {
+            run = 3f;
+        }
+
+        else
+        {
+            run = 0f;
+        }
+
+        rb.velocity = new Vector2(run, rb.velocity.y);
+    }
+}
--- New Unity Project (2)/Assets/Scripts/btn.cs
+++ New Unity Project (2)/Assets/Scripts/btn.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class btn : MonoBehaviour
+public class btn : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        transform.position = new Vector2(transform.position.x, transform.position.y + 6f);
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y - 6f);
     }
 }
