983aed5a2f7c5aeafc9961fad04a4c5456c089d2
mysmallprince <71024112+mysmallprince@users.noreply.github.com>
2020-09-21 21:19:10 +0300

    Chooselevelok

---
+++ New Unity Project (2)/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button Level2B;
+    public Button Level3B;
+    int levelComplete;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelComplete = PlayerPrefs.GetInt("LevelComplete");
+        Level2B.interactable = false;
+        Level3B.interactable = false;
+
+        switch (levelComplete)
+        {
+            case 1:
+                Level2B.interactable = true;
+                break;
+            case 2:
+                Level2B.interactable = true;
+                Level3B.interactable = true;
+                break;
+        }
+
+
+    }
+
+    public void LoadTo(int level)
+    {
+        SceneManager.LoadScene(level);
+
+    }
+
+    public void Reset()
+    {
+        Level2B.interactable = false;
+        Level3B.interactable = false;
+        PlayerPrefs.DeleteAll();
+    }
+
+}
